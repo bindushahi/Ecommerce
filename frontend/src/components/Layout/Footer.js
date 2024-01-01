e69ab3c79b52485d3c4af8335b8cc5a07d86ae80
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-02 01:07:28 +0545

    components layout(header and footer)

---
+++ frontend/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+        <h5 className='text-center'>All Right Reserved &copy;EShop2024</h5>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const layout = ({children}) => {
+  return (
+    <div>
+        <Header/>
+        <main style={{minHeight:'85vh'}}>
+                 {children}
+
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default layout
