468753bdea4bbdca03e3f2718fea0ffa6e8f92e4
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-01 18:37:46 +0545

    .env package

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express= require('express');
-const colors= require('colors');
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+
+//configure env
+dotenv.config();
+
 //rest object to call api
 const app=express();
 
 //rest api create
-app.get('/',(req,res)=>{
-    res.send({
-        message:"Welcome to Eshop",
-    });
+app.get("/",(req,res)=>{
+    res.send('<h1>Welcome to EShop</h1>');
+        // message:"Welcome to Eshop",
 });
 
 //port
-const PORT= 8080;
-
+const PORT= process.env.PORT || 8080;
 //run listen
 
 app.listen(PORT,()=>{
