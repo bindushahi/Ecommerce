196f5dd07f5e405ce6cbe984a0b83756daca35ce
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-04 02:29:11 +0545

    Helmet for seo package

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Helmet from 'react-helmet'
 
 const Layout = ({ children }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet='utf-8'/>
+        <title>My Title</title>
+      </Helmet>
       <Header />
       <div style={{ backgroundColor: 'lavender', minHeight: '80vh', padding: '20px' }}>
         {children}
+
       </div>
       <Footer />
     </div>
