bc48bd908415efb92e45024479a828a368c8e264
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-03 13:36:27 +0545

    pages create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Layout from './components/Layout/Layout';
+import {Route, Routes} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import Pagenotfound from './pages/Pagenotfound';
+
+
+
 
 function App() {
   return (
     <>
-    <Layout>
-     <h1>EShop</h1>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/about'about element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
+      <Route path='/policy' element={<Policy/>} />
+      <Route path='*' element={<Pagenotfound/>} />
 
-    </Layout>
+    </Routes>
     </>
   );
 }
