992470f099a16dafe79b119c3730c2d4fe53897b
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-05 22:05:31 +0545

    register set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
+import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
 
 
 
@@ -17,6 +19,11 @@ function App() {
       <Route path='contact' element={<Contact/>} />
       <Route path='/policy' element={<Policy/>} />
       <Route path='*' element={<Pagenotfound/>} />
+            <Route path='/register' element={<Register/>} />
+                        <Route path='/login' element={<Login/>} />
+
+
+
 
     </Routes>
     </>
---
+++ frontend/src/pages/Auth/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout.js'
+
+const Register = () => {
+  return (
+    <Layout title="Register -Ecommerce App">
+      <h1>Hello</h1>
+    </Layout>
+  )
+}
+
+export default Register
