2b5ba5f6cfb74bc1674d1129ed8fd71d81d78186
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-05 23:59:26 +0545

    state create

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Layout from '../../components/Layout/Layout.js'
 
 const Register = () => {
+    const[username,setUsername]=useState("");
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+    const[phonenumber,setPhonenumber]=useState("");
+    const[address,setAddress]=useState("");
+
   return (
+    
     <Layout title="Register -Ecommerce App">
 
 <div className='register'>
@@ -10,24 +17,24 @@ const Register = () => {
 <form>
   <div className="mb-3">
     <label htmlFor="exampleInputName" className="form-label">Username</label>
-    <input type="text" className="form-control" id="exampleInputName" placeholder='Enter Username'/>
+    <input type="text" value="username" className="form-control" id="exampleInputName" placeholder='Enter Username'/>
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-    <input type="email" className="form-control" id="exampleInputEmail1"placeholder='Enter Username'/>
+    <input type="email" value="email" className="form-control" id="exampleInputEmail1"placeholder='Enter Username'/>
   </div>
 
   <div className="mb-3">
     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"placeholder='Enter Email' />
+    <input type="password" value="password"className="form-control" id="exampleInputPassword1"placeholder='Enter Email' />
   </div>
   <div className="mb-3">
     <label htmlFor="exampleInputPhonenumber" className="form-label">Phone Number</label>
-    <input type="number" className="form-control" id="exampleInputPhonenumber"placeholder='Enter Phone Number' />
+    <input type="number" value="phonenumber"className="form-control" id="exampleInputPhonenumber"placeholder='Enter Phone Number' />
   </div>
    <div className="mb-3">
     <label htmlFor="exampleInputAddress" className="form-label">Address</label>
-    <input type="name" className="form-control" id="exampleInputAddress"placeholder='Enter Address' />
+    <input type="name"value="address" className="form-control" id="exampleInputAddress"placeholder='Enter Address' />
   </div>
 
   
