e94a2a4c657068338ca47ed55f6951ce64e83a14
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-07 23:59:20 +0545

    context api

---
+++ frontend/src/Context/Auth.js
@@ -0,0 +1,16 @@
+import { useState,useEffect,useContext,createContext } from "react";
+const AuthContext=createContext()
+
+const AuthProvider=({children})=>{
+const [auth,setAuth]=useState({
+    user:null,
+    token:""
+})
+return(
+    <AuthContext.Provider value={[auth,setAuth]}>
+        {children}
+    </AuthContext.Provider>
+)}
+//custom hook
+const useAuth=()=>useContext(AuthContext);
+export{useAuth,AuthProvider}
