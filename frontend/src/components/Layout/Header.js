6f52e8ae7e3d7d00d9787fe5f69719ddaace2e96
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-08 19:24:36 +0545

    dropdown menu create

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -50,10 +50,18 @@ const handleLogout=()=>{
         </li>
           </> ): (
             <>
-            <li className="nav-item">
-          <NavLink onClick={handleLogout} to='/login' className="nav-link">Log Out</NavLink>
-        </li>
-            
+<li className="nav-item dropdown">
+  <NavLink className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+{auth?.user?.username}
+
+  </NavLink>
+  <ul className="dropdown-menu">
+    <li><NavLink to='/dashboard'className="dropdown-item">Dashboard</NavLink></li>
+      <li>  <NavLink onClick={handleLogout} to='/login' className="dropdown-item">Log Out</NavLink></li>
+
+  
+  </ul>
+</li>       
             </>
           )}
 
