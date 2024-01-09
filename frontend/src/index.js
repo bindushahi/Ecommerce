ec88b676e23876519642f08d0bcd56507c0ae616
Bindu Shahi Thakuri <60732406+bindushahi@users.noreply.github.com>
2024-01-09 20:37:47 +0545

    antd package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter}from 'react-router-dom'
 import { AuthProvider, useAuth } from './Context/Auth';
+import 'antd/dist/reset.css';
+
 
 
 
